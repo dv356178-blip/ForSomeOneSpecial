9f49c215e625d1b8ef1e22edb79a84351aa06e55
Deepanshu Verma <dv356178@gmail.com>
2026-09-20 05:22:32 +0530

    Update app.js

--- Photos/app.js
+++ Photos/app.js
@@ -6,16 +6,16 @@ let pic4 = document.querySelector(".pic4");
 let m = document.querySelector(".photos");
 m.addEventListener("click", ()=>{
     bgmusic.play();
-})
+});
 pic1.addEventListener("click" , ()=>{
     pic1.src = "../bff-1.jpeg"
-})
+});
 pic2.addEventListener("click" , ()=>{
     pic2.src = "../bff-2.jpeg"
-})
+});
 pic3.addEventListener("click" , ()=>{
     pic3.src = "../WhatsApp Image 2026-09-19 at 10.09.30 PM.jpeg"
-})
+});
 pic4.addEventListener("click" , ()=>{
     pic4.src = "../bff-4.jpeg"
-})
+});
